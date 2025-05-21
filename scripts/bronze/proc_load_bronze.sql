bc04e1d07136da8d5201b186b5909a9a4468a912
Ben Grigsby <98256315+ben-grigsby@users.noreply.github.com>
2025-05-21 16:27:14 -0700

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -17,6 +17,8 @@ Usage Example:
 ==================================================================================================
 */
 
+EXEC bronze.load_bronze
+
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS 
 BEGIN
 	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
